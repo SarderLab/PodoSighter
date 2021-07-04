b3d90a339681e1e612126f3ed1ae7642a1a2a9b3
Darshana Govind <36133605+DarshanaGovind@users.noreply.github.com>
2021-07-04 11:17:57 -0400

    Update PodoSighter.py

--- Codes/PodoSighter.py
+++ Codes/PodoSighter.py
@@ -25,8 +25,8 @@ if not os.path.exists(tempfolder):
 
 # %%
 '''Trained models for Pix2pix'''
-#TrainedGeneratorModel = '../Data/Trained_models/pix2pix/human_p57/hum_p57_net_G.pth'
-#TrainedDiscriminatorModel = '../Data/Trained_models/pix2pix/human_p57/hum_p57_net_D.pth'
+TrainedGeneratorModel = '../Data/Trained_models/pix2pix/human_p57/hum_p57_net_G.pth'
+TrainedDiscriminatorModel = '../Data/Trained_models/pix2pix/human_p57/hum_p57_net_D.pth'
 '''OR'''
 #'''Trained models for CNN'''
 Model = '../Data/Trained_models/cnn/human_p57/hum_p57_model.ckpt-50000.data-00000-of-00001'
@@ -45,7 +45,8 @@ gpu_id=0
 resolution=0
 size_thre=400
 watershed_thre=0.2
-
+tissue_thickness=2.0#new
+csv_file_name='pod_vol_density.csv'#new
 
 
 
@@ -62,8 +63,8 @@ METHOD = 'CNN' # 'CNN' or 'pix2pix'
 '''=========='''
 
 if METHOD == 'pix2pix':
-    cmd = "python3 ./PodoSighter_pix2pix_folder/Complete_Pix2pix_Prediction.py -A0 '{}' -A1 '{}' -A2 '{}' -A3 '{}' -A4 '{}' -A5 {} -A6 {} -A7 {} -A8 '{}' -A9 {} -A10 {} -A11 {} -A12 {} -A13 '{}'".format(tempfolder,inputImageFile,inputAnnotationFile1, TrainedGeneratorModel ,TrainedDiscriminatorModel, PASnucleiThreshold,gauss_filt_size, Disc_size ,species,gpu_id,resolution,size_thre,watershed_thre,outputAnnotationFile)
+    cmd = "python3 ./PodoSighter_pix2pix_folder/Complete_Pix2pix_Prediction.py -A0 '{}' -A1 '{}' -A2 '{}' -A3 '{}' -A4 '{}' -A5 {} -A6 {} -A7 {} -A8 '{}' -A9 {} -A10 {} -A11 {} -A12 {} -A13 '{}' -A14 {} -A15 '{}'".format(tempfolder,inputImageFile,inputAnnotationFile1, TrainedGeneratorModel ,TrainedDiscriminatorModel, PASnucleiThreshold,gauss_filt_size, Disc_size ,species,gpu_id,resolution,size_thre,watershed_thre,outputAnnotationFile,tissue_thickness,csv_file_name)#new
     os.system(cmd)
 elif METHOD == 'CNN':
-    cmd = "python3 ./PodoSighter_cnn_folder/Complete_CNN_Prediction.py -A0 '{}' -A1 '{}' -A2 '{}' -A3 '{}' -A4 '{}' -A5 '{}' -A6 '{}' -A7 {} -A8 {} -A9 {} -A10 {} -A11 {} -A12 {} -A13 '{}'".format(tempfolder,inputImageFile,inputAnnotationFile1, Model,Modelchkpt,Modelidx, species, PASnucleiThreshold,gauss_filt_size, Disc_size,resolution,size_thre,watershed_thre,outputAnnotationFile)
+    cmd = "python3 ./PodoSighter_cnn_folder/Complete_CNN_Prediction.py -A0 '{}' -A1 '{}' -A2 '{}' -A3 '{}' -A4 '{}' -A5 '{}' -A6 '{}' -A7 {} -A8 {} -A9 {} -A10 {} -A11 {} -A12 {} -A13 '{}' -A14 {} -A15 '{}'".format(tempfolder,inputImageFile,inputAnnotationFile1, Model,Modelchkpt,Modelidx, species, PASnucleiThreshold,gauss_filt_size, Disc_size,resolution,size_thre,watershed_thre,outputAnnotationFile,tissue_thickness,csv_file_name)
     os.system(cmd)    
